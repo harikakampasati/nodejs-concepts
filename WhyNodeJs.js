aaee7fbca61724188a4943458e205f18c50d4eb6
HarikaBottaNpiraNarayanaGroup <harika.botta@narayanagroup.com>
2024-02-17 02:37:32 +0530

    why node js

---
+++ WhyNodeJs.js
@@ -0,0 +1,10 @@
+/**
+ * why node js is such a good fit for backend development and where we should use it and where not?
+ * 1. the reason why the node js applications are so fast so scalble because node js is single threaded based on event driven non blocking IO model
+ * which makes node js is very fit and efficient.
+ * 2. node js is perfect for building fast and scalble data intensive application
+ * 3. many top companies like netflix, uber, paypal, epay  uses the node js
+ * 4. the main advantage of using node js is that since we are using javascript as the programming language, we can use same technology acrose the entire stack
+ * that means we can use javascript for frontend and the backend this is lot easier for developers
+ * 5. there is huge library  open source packages are available (these package ara available on npm  node package manager) 
+ */
