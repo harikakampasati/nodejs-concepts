b7e482ec0037c00b2d77734649df2b76e79e0180
HarikaBottaNpiraNarayanaGroup <harika.botta@narayanagroup.com>
2024-02-16 08:26:13 +0530

    info to the node js

---
+++ infoNodeJs.js
@@ -0,0 +1,19 @@
+/**
+ * Node js is javascript runtime environment built on Google's open source v8 engine
+ * it uses googles v8 engine.
+ * any browser natively understand the html css js no matter if it is venilla js or js framework react js etc
+ * browser provides all the tools  and libraries to execute and run javascript code
+ * browser also provide some of the functions like console.log(), fetch api, querySelector(), getElementById() etc.. we use them while execute js code
+ * before 2009 javascript has only capable of running inside the browser. there is no way to run javascript program outside of the browser environment
+ * javascript engine is a tool required by the browser, every browser there is a javascript engine, which is responsible of execute javascript code.
+ * every browser there has their own javascript engine for example :- firefox: spider monkey, microsoft edge: chakra, 
+ * in the same way the javascript engine of the google chrome is v8 engine. 
+ * the v8 engine is the responsible executing javascript code in the chrome browser
+ * before 2009 it was not possible to do execute javascript program outside of the browser 
+ * but in 2009 a programmer named as Ryan Dahl creator of node.js, came out with a brillient idea that google he took v8 engine embedded in C++ program and he called it as node js.
+ * same as simillar to a browser, node js a run time environment to execute a javascript code. and it contain the javascript engine (V8 engine) which can execute a javascript code.
+ * that why we say that nodejs is the javascript runtime environment which is bult google v8 javascript engine
+ * node js has some additional apis that gives as capability to run javascript outside of the browser. (examples http(), fs, readline, console.log)
+ * now we can do so many thing javascript which was completely impossible before. ( for example accessing file system etc..)
+ * all these factors together gives us the perfect condition for using node js a service side language means we can use javascript as service side of the web development in order build fast highly scalable network application for the backend of the websites
+ */
